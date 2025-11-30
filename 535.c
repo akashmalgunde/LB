82596362b74ad2679676acc05680fe31ddaa764c
akashmalgunde <akashmalgunde56@gmail.com>
2025-11-30 11:21:18 +0530

    Add files via upload

---
+++ 535.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+void Display(int iRows,int iColumns)
+{
+    for(int i = 1; i <= iRows; i++)
+    {
+        for(int j = 1; j <= iColumns; j++)
+        {
+            if(i == 1 || i == iRows || j == 1 || j == iColumns)
+                printf("$ ");
+
+            else if(i == j)
+                printf("* ");
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iRows = 0;
+    int iColumn = 0;
+
+    printf("Enter the no of rows:\n");
+    scanf("%d",&iRows);
+
+    printf("Enter the no of columns:\n");
+    scanf("%d",&iColumn);
+
+    Display(iRows,iColumn);
+
+    return 0;
+}
---
+++ 538.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+void Display(int iRows,int iColumns)
+{
+    for(int i = 1; i <= iRows; i++)
+    {
+        for(int j = 1; j <= iColumns; j++)
+        {
+            if(i == j)
+                printf("* ");
+
+            else if(j < i)
+                printf("$ ");
+
+            else
+                printf("@ ");
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iRows = 0;
+    int iColumn = 0;
+
+    printf("Enter the no of rows:\n");
+    scanf("%d",&iRows);
+
+    printf("Enter the no of columns:\n");
+    scanf("%d",&iColumn);
+
+    Display(iRows,iColumn);
+
+    return 0;
+}
