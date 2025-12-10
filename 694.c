185cefc50d8428f0fa5388a281f57090c29330e6
akashmalgunde <akashmalgunde56@gmail.com>
2025-12-10 12:11:06 +0530

    Add files via upload

---
+++ 694.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+void Display(int iRows)
+{
+    int iCnt1 = 1;
+    int iCnt2 = iRows*2 - 1;
+    for(int i = 1; i <= iRows; i++,iCnt1++,iCnt2--)
+    {
+        for(int j = 1; j < iRows*2; j++)
+            if(j >= iCnt1 && j <= iCnt2)
+                printf("* ");
+            else 
+                printf("  ");
+
+        printf("\n");
+    }
+    iCnt1 -= 2;
+    iCnt2 += 2;
+    for(int i = 1; i < iRows; i++,iCnt1--,iCnt2++)
+    {
+        for(int j = 1; j < iRows*2; j++)
+            if(j >= iCnt1 && j <= iCnt2)
+                printf("* ");
+            else 
+                printf("  ");
+
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iRows = 0;
+
+    printf("Enter rows:\n");
+    scanf("%d",&iRows);
+
+    Display(iRows);
+
+    return 0;
+}
